46149a79137b43a5a48ea3b02c86b8364079bf7c
Tajir-Ramadhan <tajiriramadhan@gmail.com>
2022-09-08 15:27:42 -0700

    advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
+#include <unistd.h>
 . /*
  * File: 101-quote.c
- * Auth: Brennan D Baraban
+ * Auth: Brennan D Baraba
+ *
  */
-
-#include <unistd.h>
-
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
