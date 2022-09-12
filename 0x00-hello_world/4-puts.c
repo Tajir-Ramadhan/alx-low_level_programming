b853d3711903930a9824aa0dba79743ef5b52e40
Tajir-Ramadhan <tajiriramadhan@gmail.com>
2022-09-12 13:15:51 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-	/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/**
+ * main - main block
+ * Return: 0
  */
 int main(void)
-	       /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
