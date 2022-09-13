a383dec6bf1530d30472729d47a61491025d986b
Tajir-Ramadhan <tajiriramadhan@gmail.com>
2022-09-13 16:34:30 +0300

    starting

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * Fnction declaration
+ * main function starts
+ *The function must print characters in upper case followed by low case
+ *Fungtion must reun value
+ *Return (0)
+ */
+void print_alphabet(void);
+{
+char char;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This readme briefly explains about this tasks.
+abvuously these projects conserns about c nested loops and finction
